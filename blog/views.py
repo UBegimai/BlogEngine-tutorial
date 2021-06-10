4baf4f3da14acfee2d7dbcd7376c62a202f1cdd9
UBegimai <umeibegimai@gmail.com>
2021-06-10 23:50:02 +0600

    Class Based Views (CBVs) и использование Миксинов

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,29 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.views import View
+
 from blog.models import Post, Tag
+from .utils import ObjectDetailMixin
 
 
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
 
 def tags_list(request):
     tags = Tag.objects.all()
-    return render(request, 'blog/tags_list.html', context={'tags': tags})
-
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+    return render(request, 'blog/tags_list.html',
+                  context={'tags': tags})
