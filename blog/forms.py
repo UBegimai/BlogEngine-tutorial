fcbc971974ed3e08c0da03bc9b5fe2542e9dffeb
UBegimai <umeibegimai@gmail.com>
2021-06-11 10:38:21 +0600

    Создание Постов через форму, генерация слага, Миксин

--- blog/forms.py
+++ blog/forms.py
@@ -1,15 +1,10 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import Tag
+from .models import Tag, Post
 
 
 class TagForm(forms.ModelForm):
-    # title = forms.CharField(max_length=50)
-    # slug = forms.SlugField(max_length=50)
-    #
-    # title.widget.attrs.update({'class': 'form-control'})
-    # slug.widget.attrs.update({'class': 'form-control'})
     class Meta:
         model = Tag
         fields = ['title', 'slug']
@@ -26,6 +21,25 @@ class TagForm(forms.ModelForm):
             raise ValidationError('Slug may not be "Create"')
         if Tag.objects.filter(slug__iexact=new_slug).count():
             raise ValidationError('Slug must be unique. We have "{}" slug already'.format(new_slug))
+        return new_slug
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'body', 'tags']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'form-control'}),
+        }
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "Create"')
         return new_slug
 
 
--- blog/utils.py
+++ blog/utils.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
+
 
 class ObjectDetailMixin:
     model = None
@@ -9,3 +10,20 @@ class ObjectDetailMixin:
         obj = get_object_or_404(self.model, slug__iexact=slug)
         return render(request, self.template,
                       context={self.model.__name__.lower(): obj})
+
+
+class ObjectCreateMixin:
+    model_form = None
+    template = None
+
+    def get(self, request):
+        form = self.model_form()
+        return render(request, self.template, context={'form': form})
+
+    def post(self, request):
+        bound_form = self.model_form(request.POST)
+
+        if bound_form.is_valid():
+            new_obj = bound_form.save()
+            return redirect(new_obj)
+        return render(request, self.template, context={'form': bound_form})
