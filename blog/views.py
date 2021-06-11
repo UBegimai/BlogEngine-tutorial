a3d4a1c39301522aa92746c8a203713ba8ff2318
UBegimai <umeibegimai@gmail.com>
2021-06-11 11:45:58 +0600

    Изменение моделей Post, Tag через форму и Миксин

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.views import View
 
 from .models import Post, Tag
 from .forms import TagForm, PostForm
-from .utils import ObjectDetailMixin, ObjectCreateMixin
+from .utils import *
 
 
 def posts_list(request):
@@ -20,15 +20,26 @@ class PostCreate(ObjectCreateMixin, View):
     model_form = PostForm
     template = 'blog/post_create_form.html'
 
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
+
 
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
 
+
 class TagCreate(ObjectCreateMixin, View):
     model_form = TagForm
     template = 'blog/tag_create.html'
 
+
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
 
 
 def tags_list(request):
