2a53ecfc28e9f9ed7cb2352f3bde4af000a5680b
UBegimai <umeibegimai@gmail.com>
2021-06-12 22:03:43 +0600

    Простой поиск по заголовкам и телу постов

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,48 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 
 from .models import Post, Tag
 from .forms import TagForm, PostForm
 from .utils import *
-
+from django.core.paginator import Paginator
 
 def posts_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/index.html', context={'posts': posts})
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) |
+                                    Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+
+    context = {
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'next_url': next_url,
+        'prev_url': prev_url
+    }
+
+
+
+    return render(request, 'blog/index.html', context=context)
 
 
 class PostDetail(ObjectDetailMixin, View):
